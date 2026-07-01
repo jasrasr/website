01cd6a0a6c5592f9904c6ac85b255612430f9f98
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-06-30 20:30:17 -0400

    Show GPS ETA trip session revision

--- gps-eta/index-secure.php
+++ gps-eta/index-secure.php
@@ -13,6 +13,14 @@ ob_start();
 require __DIR__ . '/index.php';
 $html = ob_get_clean();
 
+if (is_string($html)) {
+    $html = str_replace(
+        'Rev 1.5.0 &bull; Updated 2026-06-30 &bull; Per-Device History + 365-Day Retention',
+        'Rev 1.6.0 &bull; Updated 2026-06-30 &bull; Trip Sessions + End Trip Summary',
+        $html
+    );
+}
+
 $scriptTag = '<script src="ui-render.js?v=1.5.1"></script><script src="trip-sessions.js?v=1.6.0"></script>';
 
 if (is_string($html) && strpos($html, '</body>') !== false) {
