3c2b477053219b1a7e888653c43f6096171e81ce
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-03 20:48:47 -0400

    Refresh service worker cache for automatic episode refresh

--- tv-binge-board/service-worker.js
+++ tv-binge-board/service-worker.js
@@ -5,10 +5,10 @@
  * Author: Jason Lamb / ChatGPT
  * Created: 2026-07-02
  * Modified: 2026-07-03
- * Revision: 1.5.8
+ * Revision: 1.5.9
  */
 
-const CACHE_NAME = 'tv-binge-board-rev-1.5.8';
+const CACHE_NAME = 'tv-binge-board-rev-1.5.9';
 const OFFLINE_URL = 'offline.php';
 const SHELL_ASSETS = [
     './',
