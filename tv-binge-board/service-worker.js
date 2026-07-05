ab6ce9f674dde2f50748d8eb0060cf30245d560e
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-05 10:47:50 -0400

    Refresh service worker cache for friend activity feed

--- tv-binge-board/service-worker.js
+++ tv-binge-board/service-worker.js
@@ -5,10 +5,10 @@
  * Author: Jason Lamb / ChatGPT
  * Created: 2026-07-02
  * Modified: 2026-07-05
- * Revision: 1.5.15
+ * Revision: 1.5.16
  */
 
-const CACHE_NAME = 'tv-binge-board-rev-1.5.15';
+const CACHE_NAME = 'tv-binge-board-rev-1.5.16';
 const OFFLINE_URL = 'offline.php';
 const SHELL_ASSETS = [
     './',
