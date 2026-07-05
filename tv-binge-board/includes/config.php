0d1eb73c9cf669daa5cc9ddfb89eb59b90ebfffb
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-05 12:53:18 -0400

    Bump project revision to 1.5.17

--- tv-binge-board/includes/config.php
+++ tv-binge-board/includes/config.php
@@ -6,13 +6,13 @@
  * Author: Jason Lamb / ChatGPT
  * Created: 2026-07-02
  * Modified: 2026-07-05
- * Revision: 1.5.16
+ * Revision: 1.5.17
  */
 declare(strict_types=1);
 
 
 const APP_NAME = 'TV Binge Board';
-const APP_VERSION = '1.5.16';
+const APP_VERSION = '1.5.17';
 const APP_TIMEZONE = 'America/New_York';
 const APP_SESSION_NAME = 'tv_binge_board_session';
 const APP_DEFAULT_POSTER = 'assets/img/poster-placeholder.svg';
