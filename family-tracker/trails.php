fc4a055c46d7937b864c5df8eb6b73a2a5641df7
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-06 20:55:53 -0400

    Audit family tracker headers and placeholders

--- family-tracker/trails.php
+++ family-tracker/trails.php
@@ -2,7 +2,7 @@
 /**
  * Project: Family GPS Tracker
  * File: trails.php
- * Revision: 0.2.0
+ * Revision: 0.2.1
  * Description: Shared family trail-history endpoint.
  * Author: Jason Lamb / ChatGPT scaffold
  * Created: 2026-07-06
