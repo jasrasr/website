54c3d4c880bd910ffe6db4258bf0a3cafaefbfa6
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-11 09:36:57 -0400

    Bump revision for invite lifecycle and group deletion

--- family-tracker/includes/config.php
+++ family-tracker/includes/config.php
@@ -2,7 +2,7 @@
 /**
  * Project: Family GPS Tracker
  * File: includes/config.php
- * Revision: 1.4.7
+ * Revision: 1.4.8
  * Description: Central application configuration.
  * Author: Jason Lamb / ChatGPT scaffold
  * Created: 2026-07-06
@@ -12,9 +12,9 @@
 declare(strict_types=1);
 
 const APP_NAME = 'Family GPS Tracker';
-const APP_REVISION = '1.4.7';
+const APP_REVISION = '1.4.8';
 const APP_UPDATED = '2026-07-11';
-const APP_BUILD_LABEL = '2026-07-11 10:30 ET';
+const APP_BUILD_LABEL = '2026-07-11 11:15 ET';
 
 define('DATA_DIR', realpath(__DIR__ . '/../data') ?: (__DIR__ . '/../data'));
 
