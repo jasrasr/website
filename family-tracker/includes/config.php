ddd9723cf48823e5c48fad309cba12a552a66355
Jason Lamb <92162022+jasrasr@users.noreply.github.com>
2026-07-13 07:59:09 -0400

    Bump revision for geofence editing

--- family-tracker/includes/config.php
+++ family-tracker/includes/config.php
@@ -2,7 +2,7 @@
 /**
  * Project: Family GPS Tracker
  * File: includes/config.php
- * Revision: 1.6.0
+ * Revision: 1.6.1
  * Description: Central application configuration.
  * Author: Jason Lamb / ChatGPT scaffold
  * Created: 2026-07-06
@@ -12,9 +12,9 @@
 declare(strict_types=1);
 
 const APP_NAME = 'Family GPS Tracker';
-const APP_REVISION = '1.6.0';
+const APP_REVISION = '1.6.1';
 const APP_UPDATED = '2026-07-13';
-const APP_BUILD_LABEL = '2026-07-13 08:45 ET';
+const APP_BUILD_LABEL = '2026-07-13 09:30 ET';
 const CONSENT_VERSION = '2026-07-11';
 const LOGIN_THROTTLE_MAX_FAILURES = 5;
 const LOGIN_THROTTLE_WINDOW_SECONDS = 900;
